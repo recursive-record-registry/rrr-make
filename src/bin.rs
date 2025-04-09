c05222d66bba08e5ce515d9e91d94c195fcde405
Jakub Hlusička <Limeth@users.noreply.github.com>
2025-04-10 00:42:06 +0200

    Update to edition 2024

--- src/bin.rs
+++ src/bin.rs
@@ -2,18 +2,17 @@ use clap::Parser;
 use color_eyre::eyre::Result;
 use rrr_make::cmd::Command;
 use tracing_error::ErrorLayer;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+use tracing_subscriber::{EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
 
 async fn setup_tracing() -> Result<()> {
-    // Enable logging by default.
-    if std::env::var("RUST_LOG").is_err() {
-        std::env::set_var("RUST_LOG", format!("{}=info", env!("CARGO_CRATE_NAME")));
-    }
+    let log_directives = std::env::var("RUST_LOG")
+        .ok()
+        .unwrap_or_else(|| format!("{}=info", env!("CARGO_CRATE_NAME")));
 
     tracing_subscriber::registry()
         .with(tracing_subscriber::fmt::layer())
         .with(ErrorLayer::default())
-        .with(EnvFilter::from_default_env())
+        .with(EnvFilter::builder().parse_lossy(log_directives))
         .try_init()?;
 
     Ok(())
